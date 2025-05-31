6cb2f16e1279bdc4b9ca41ab9e565a510651254b
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2025-06-01 01:08:09 +0200

    Corrected some chords

--- src/extract_chord.py
+++ src/extract_chord.py
@@ -25,8 +25,8 @@ CHORD_DEGREE_MAP: dict[str, list[Degree]] = {
     "aug(add13)": [Degree.R, Degree.M3, Degree.m6, Degree.M6],
     # Settima
     "7": [Degree.R, Degree.M3, Degree.P5, Degree.m7],
-    "m7": [Degree.R, Degree.m3, Degree.P5, Degree.m7],
-    "m7b5": [Degree.R, Degree.m3, Degree.A4, Degree.m7],
+    "min7": [Degree.R, Degree.m3, Degree.P5, Degree.m7],
+    "min7b5": [Degree.R, Degree.m3, Degree.A4, Degree.m7],
     "maj7": [Degree.R, Degree.M3, Degree.P5, Degree.M7],
     "dim7": [Degree.R, Degree.m3, Degree.A4, Degree.M6],  # M6 = dim7 enharmonic
     "minMaj7": [Degree.R, Degree.m3, Degree.P5, Degree.M7],
@@ -39,17 +39,9 @@ CHORD_DEGREE_MAP: dict[str, list[Degree]] = {
     # Estensioni
     "maj9": [Degree.R, Degree.M3, Degree.P5, Degree.M7, Degree.M9],
     "9": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.M9],
-    "m9": [Degree.R, Degree.m3, Degree.P5, Degree.m7, Degree.M9],
+    "min9": [Degree.R, Degree.m3, Degree.P5, Degree.m7, Degree.M9],
     "11": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.M9, Degree.P4],
-    "13": [
-        Degree.R,
-        Degree.M3,
-        Degree.P5,
-        Degree.m7,
-        Degree.M9,
-        Degree.P4,
-        Degree.M6,
-    ],
+    "13": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.M9, Degree.P4, Degree.M6],
     "maj13": [
         Degree.R,
         Degree.M3,
@@ -59,25 +51,19 @@ CHORD_DEGREE_MAP: dict[str, list[Degree]] = {
         Degree.P4,
         Degree.M6,
     ],
-    "m6": [
-        Degree.R,
-        Degree.m3,
-        Degree.P5,
-        Degree.m7,
-        Degree.M9,
-        Degree.P4,
-        Degree.M6,
-    ],
+    "6": [Degree.R, Degree.M3, Degree.P5, Degree.M6],
+    "min6": [Degree.R, Degree.m3, Degree.P5, Degree.M6],
+    "6/9": [Degree.R, Degree.M3, Degree.P5, Degree.M6, Degree.M9],
     # Accordi alterati
-    "7b9": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m9],
-    "7#9": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m3],
-    "7b5": [Degree.R, Degree.M3, Degree.A4, Degree.m7],
-    "7#5": [Degree.R, Degree.M3, Degree.m6, Degree.m7],
-    "7b9b5": [Degree.R, Degree.M3, Degree.A4, Degree.m7, Degree.m9],
-    "7b9#5": [Degree.R, Degree.M3, Degree.m6, Degree.m7, Degree.m9],
-    "7#9b5": [Degree.R, Degree.M3, Degree.A4, Degree.m7, Degree.m3],
-    "7#9#5": [Degree.R, Degree.M3, Degree.m6, Degree.m7, Degree.m3],
-    "13b9": [
+    "7(b9)": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m9],
+    "7(#9)": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m3],
+    "7(b5)": [Degree.R, Degree.M3, Degree.A4, Degree.m7],
+    "7(#5)": [Degree.R, Degree.M3, Degree.m6, Degree.m7],
+    "7(b9, b5)": [Degree.R, Degree.M3, Degree.A4, Degree.m7, Degree.m9],
+    "7(b9, #5)": [Degree.R, Degree.M3, Degree.m6, Degree.m7, Degree.m9],
+    "7(#9, b5)": [Degree.R, Degree.M3, Degree.A4, Degree.m7, Degree.m3],
+    "7(#9, #5)": [Degree.R, Degree.M3, Degree.m6, Degree.m7, Degree.m3],
+    "13(b9)": [
         Degree.R,
         Degree.M3,
         Degree.P5,
@@ -86,7 +72,7 @@ CHORD_DEGREE_MAP: dict[str, list[Degree]] = {
         Degree.P4,
         Degree.M6,
     ],
-    "13#9": [
+    "13(#9)": [
         Degree.R,
         Degree.M3,
         Degree.P5,
@@ -95,35 +81,41 @@ CHORD_DEGREE_MAP: dict[str, list[Degree]] = {
         Degree.P4,
         Degree.M6,
     ],
-    "7#11": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.M9, Degree.A4],
-    "7b13": [
-        Degree.R,
-        Degree.M3,
-        Degree.P5,
-        Degree.m7,
-        Degree.M9,
-        Degree.P4,
-        Degree.m6,
-    ],
+    "7(#11)": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.A4],
+    "7(b13)": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m6],
 }
 
 CHORD_DEGREE_MAP.update(
     {
         # Accordi di settima ed estensioni variate
-        "maj7#11": [Degree.R, Degree.M3, Degree.P5, Degree.M7, Degree.M9, Degree.A4],
+        "maj7(#11)": [Degree.R, Degree.M3, Degree.P5, Degree.M7, Degree.A4],
         "maj7add13": [Degree.R, Degree.M3, Degree.P5, Degree.M7, Degree.M6],
-        "mMaj7": [Degree.R, Degree.m3, Degree.P5, Degree.M7],
-        "mMaj9": [Degree.R, Degree.m3, Degree.P5, Degree.M7, Degree.M9],
-        "mMaj7add11": [Degree.R, Degree.m3, Degree.P5, Degree.M7, Degree.P4],
+        "minMaj7": [Degree.R, Degree.m3, Degree.P5, Degree.M7],
+        "minMaj9": [Degree.R, Degree.m3, Degree.P5, Degree.M7, Degree.M9],
+        "minMaj7add11": [Degree.R, Degree.m3, Degree.P5, Degree.M7, Degree.P4],
         # Accordi di nona alterata
-        "7b9#11": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m9, Degree.A4],
-        "7#9#11": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m3, Degree.A4],
-        "7b9b13": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m9, Degree.m6],
-        "7#9b13": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m3, Degree.m6],
-        "7b9#5#11": [Degree.R, Degree.M3, Degree.m6, Degree.m7, Degree.m9, Degree.A4],
-        "7#9#5b13": [Degree.R, Degree.M3, Degree.m6, Degree.m7, Degree.m3, Degree.m6],
-        # Accordi di 11 alterati
-        "13#11": [
+        "7(b9, #11)": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m9, Degree.A4],
+        "7(#9, #11)": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m3, Degree.A4],
+        "7(b9, b13)": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m9, Degree.m6],
+        "7(#9, b13)": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.m3, Degree.m6],
+        "7(b9, #5, #11)": [
+            Degree.R,
+            Degree.M3,
+            Degree.m6,
+            Degree.m7,
+            Degree.m9,
+            Degree.A4,
+        ],
+        "7(#9, #5, b13)": [
+            Degree.R,
+            Degree.M3,
+            Degree.m6,
+            Degree.m7,
+            Degree.m3,
+            Degree.m6,
+        ],
+        # Accordi alterati
+        "13(#11)": [
             Degree.R,
             Degree.M3,
             Degree.P5,
@@ -132,31 +124,20 @@ CHORD_DEGREE_MAP.update(
             Degree.A4,
             Degree.M6,
         ],
-        "m6b9": [
-            Degree.R,
-            Degree.m3,
-            Degree.P5,
-            Degree.m7,
-            Degree.m9,
-            Degree.P4,
-            Degree.M6,
-        ],
-        "m11": [Degree.R, Degree.m3, Degree.P5, Degree.m7, Degree.M9, Degree.P4],
-        "m11b5": [Degree.R, Degree.m3, Degree.A4, Degree.m7, Degree.M9, Degree.P4],
+        "min6(b9)": [Degree.R, Degree.m3, Degree.P5, Degree.M6, Degree.m9],
+        "min11": [Degree.R, Degree.m3, Degree.P5, Degree.m7, Degree.M9, Degree.P4],
+        "min11(b5)": [Degree.R, Degree.m3, Degree.A4, Degree.m7, Degree.M9, Degree.P4],
         # Add accordi con settima
         "7add13": [Degree.R, Degree.M3, Degree.P5, Degree.m7, Degree.M6],
-        "m7add11": [Degree.R, Degree.m3, Degree.P5, Degree.m7, Degree.P4],
-        "m7add13": [Degree.R, Degree.m3, Degree.P5, Degree.m7, Degree.M6],
+        "min7add11": [Degree.R, Degree.m3, Degree.P5, Degree.m7, Degree.P4],
+        "min7add13": [Degree.R, Degree.m3, Degree.P5, Degree.m7, Degree.M6],
         "maj7add9": [Degree.R, Degree.M3, Degree.P5, Degree.M7, Degree.M9],
         # Accordi sus estesi
         "7sus2": [Degree.R, Degree.M9, Degree.P5, Degree.m7],
         "9sus4": [Degree.R, Degree.P4, Degree.P5, Degree.m7, Degree.M9],
         "13sus4": [Degree.R, Degree.P4, Degree.P5, Degree.m7, Degree.M9, Degree.M6],
         # Accordi 6 e varianti
-        "6": [Degree.R, Degree.M3, Degree.P5, Degree.M6],
-        "m6": [Degree.R, Degree.m3, Degree.P5, Degree.M6],
-        "6add9": [Degree.R, Degree.M3, Degree.P5, Degree.M6, Degree.M9],
-        "m6add9": [Degree.R, Degree.m3, Degree.P5, Degree.M6, Degree.M9],
+        "min6add9": [Degree.R, Degree.m3, Degree.P5, Degree.M6, Degree.M9],
     }
 )
 
